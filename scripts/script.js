e5d39b42bc572225faa5acc14ba0d46ff54bf90b
Augustosanttos <josesanttos123456@gmail.com>
2025-01-06 18:15:13 -0300

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-function blurInput(input){
-    const idlabel = 'label-' + input.name
-    const isValue = input.value !== ''
-
-    const label = document.getElementById(idlabel)
-    label.classList = isValue ? 'active' : ''
-}
